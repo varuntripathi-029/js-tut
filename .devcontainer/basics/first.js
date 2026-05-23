a915e4ff90aa1f7d8aa581085fdcfa4637173c31
varuntripathi-029 <159700481+varuntripathi-029@users.noreply.github.com>
2026-05-23 06:52:20 +0000

    fixed size of fonts

---
+++ .devcontainer/basics/first.js
@@ -0,0 +1,8 @@
+var accountPassword = "myPassword123";
+var accountEmail = "myEmail@example.com";
+const accountUsername = "myUsername";
+
+console.log("Account Information:");
+console.log("Username: " + accountUsername);
+console.log("Email: " + accountEmail);
+console.log("Password: " + accountPassword);
