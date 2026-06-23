bf10a8cc5ba69267c90f163d04d4dc1fad87a08d
varuntripathi-029 <159700481+varuntripathi-029@users.noreply.github.com>
2026-06-23 07:50:30 +0000

    add sum2 function to demonstrate rest parameters with additional arguments

--- .devcontainer/basics/restPara.js
+++ .devcontainer/basics/restPara.js
@@ -5,3 +5,7 @@ function sum(...numbers) {
 
 console.log(sum(1, 2, 3)); // Output: 6
 console.log(sum(4, 5));    // Output: 9
+function sum2(a, b, ...numbers) {
+    return numbers
+}
+console.log(sum2(1, 2, 3, 4, 5)); // Output: [3, 4, 5] bcz the first two parameters a and b are assigned the values 1 and 2, respectively, and the rest of the arguments (3, 4, 5) are collected into the numbers array.
