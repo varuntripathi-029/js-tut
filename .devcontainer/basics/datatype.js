840f87c482b83624663344b50eaab3d54b948ff2
varuntripathi-029 <159700481+varuntripathi-029@users.noreply.github.com>
2026-06-23 07:32:59 +0000

    add datatype.js with initial variable declarations and console logs

---
+++ .devcontainer/basics/datatype.js
@@ -0,0 +1,10 @@
+const  a = 10;
+let Object = {
+    name: "John",
+    age: a
+};
+const arr = [1, 2, 3, 4, 5];
+
+console.log("Integer:", a);
+console.log("Object:", Object);
+console.log("Array:", arr);
